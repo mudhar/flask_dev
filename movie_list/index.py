bce2b280bf9fbc692ef323305f0ed839139d6c60
rizki <mudharrizki@gmail.com>
2020-12-23 20:07:47 +0700

    list movie from api key

--- movie_list/index.py
+++ movie_list/index.py
@@ -1,3 +1,15 @@
 from flask import Flask, render_template,request
 import requests
 
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    raw_data = requests.get("http://www.omdbapi.com/?i=tt3896198&apikey=e8a201e0")
+    movies = raw_data.json()
+    return render_template("home.html", movies=movies)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
